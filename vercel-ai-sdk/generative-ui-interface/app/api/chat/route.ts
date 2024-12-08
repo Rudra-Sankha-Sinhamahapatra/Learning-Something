5b312a4aa73e47fa7c198e2a51b98301bb881504
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-12-08 22:02:21 +0530

    changing model

--- vercel-ai-sdk/generative-ui-interface/app/api/chat/route.ts
+++ vercel-ai-sdk/generative-ui-interface/app/api/chat/route.ts
@@ -17,7 +17,7 @@ export async function POST(request:Request) {
     const {messages} = await request.json();
 
     const result = streamText({
-        model:google('gemini-1.5-pro'),
+        model:google('gemini-1.5-flash-8b'),
         system:"You are a friendly assistant!",
         messages,
         maxSteps:5,

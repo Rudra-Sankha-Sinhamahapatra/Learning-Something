e277c1999b5f0cfda3b7284492d89fb03ee171a4
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-12-23 02:11:09 +0530

    init

--- aws_s3/getObjects_backend1/src/index.ts
+++ aws_s3/getObjects_backend1/src/index.ts
@@ -9,30 +9,27 @@ app.post('/getObject',async(req:Request,res:Response): Promise<any> =>{
     const { objectName }= req.body;
 
     if (!objectName) {
-         res.status(400).json({
+       return  res.status(400).json({
             message: "objectName is required"
         });
-        return;
     }
 
     
     try{
         const url = await getObjectURL(objectName);
 
-        res.status(200).json({
+        return res.status(200).json({
             status:"Success",
             message:"URL will be expired after 20 seconds",
             url
         })
-        return;
     }
     catch(error){
         console.error(error);
-       res.status(500).json({
+       return res.status(500).json({
             message:"Internal server error",
             error
         })
-        return;
     }
 
 })

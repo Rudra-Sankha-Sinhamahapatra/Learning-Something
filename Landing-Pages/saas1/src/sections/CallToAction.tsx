c9cd9f687d2b019e9c9a3ad65ad621e9fed83878
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2025-03-31 03:03:19 +0530

    footer and call to action

--- Landing-Pages/saas1/src/sections/CallToAction.tsx
+++ Landing-Pages/saas1/src/sections/CallToAction.tsx
@@ -1,3 +1,25 @@
+import starImage from "@/assets/star.png";
+import springImage from "@/assets/spring.png";
+import Image from "next/image";
+import ArrowRight from "@/assets/arrow-right.svg";
 export const CallToAction = () => {
-  return null;
+  return <section className="py-24 bg-gradient-to-b from-white to-[#D2DCFF] overflow-x-clip">
+    <div className="container">
+      <div className="md:flex flex-row justify-center w-full">
+      <div className="md:flex flex-col relative">
+        <h1 className="heading-1">Sign up for free today</h1>
+        <p className="description-1 mt-5 max-w-[550px]">
+        Celebrate the joy of accomplishment with an app designed to track your progress, motivate your efforts, and celebrate your success
+        </p>
+        <Image src={starImage} alt="star" className="hidden md:block h-72 w-72 absolute -left-[250px] -top-[100px]"/>
+        <div className="flex gap-4 mt-8 justify-center">
+          <button className="btn btn-primary">Get for Free</button>
+          <button className="btn btn-text gap-1">Learn More <ArrowRight className="h-5 w-5 my-auto"/></button>
+        </div>
+        <Image src={springImage} alt="spring" className="hidden md:block h-72 w-72 absolute -right-[250px] -bottom-[100px]"/>
+        </div>
+      </div>
+
+    </div>
+  </section>;
 };
--- Landing-Pages/saas1/src/sections/Footer.tsx
+++ Landing-Pages/saas1/src/sections/Footer.tsx
@@ -1,3 +1,62 @@
+import Logo from "@/assets/logosaas.png"
+import Image from "next/image"
+import instagram from "@/assets/social-insta.svg"
+import linkedin from "@/assets/social-linkedin.svg"
+import pin from "@/assets/social-pin.svg"
+import x from "@/assets/social-x.svg"
+import youtube from "@/assets/social-youtube.svg"
+
 export const Footer = () => {
-  return null;
+  const socialIcons = [
+    {
+      src: instagram,
+      alt: "instagram",
+    },
+    {
+      src: linkedin,
+      alt: "linkedin",
+    },
+    {
+      src: pin,
+      alt: "pin",
+    },
+    {
+      src: x,
+      alt: "x",
+    },
+    {
+      src: youtube,
+      alt: "youtube",
+    }
+  ]
+
+  return <footer className="bg-black text-white py-20">
+    <div className="container">
+      <div className="flex flex-col justify-center items-center">
+        <div className="inline-flex rounded-md relative before:absolute before:content-[''] before:top-2 before:left-0 before:right-0 before:bottom-0 before:blur-xl before:bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD99,#C2F0B1,#2FDBFE)]">
+          <Image src={Logo} alt="logo" className="h-12 w-auto relative z-10"/>
+        </div>
+        <div className="flex max-md:flex-col max-md:justify-center gap-8 mt-8">
+          <a href="#" className="text-white/80 transition-colors">About</a>
+          <a href="#" className="text-white/80 transition-colors">Pricing</a>
+          <a href="#" className="text-white/80 transition-colors">Contact</a>
+          <a href="#" className="text-white/80 transition-colors">Terms</a>
+          <a href="#" className="text-white/80 transition-colors">Privacy</a>
+          <a href="#" className="text-white/80 transition-colors">Features</a>
+        </div>
+        <div className="flex gap-6 mt-8">
+          {socialIcons.map((icon, index) => (
+            <a 
+              href="#" 
+              key={index}
+              className="h-10 w-10 flex items-center justify-center rounded-full"
+            >
+              <icon.src className="h-8 w-8"/>
+            </a>
+          ))}
+        </div>
+        <p className="text-white/80  text-lg mt-8 tracking-tight">© 2025 Rudra Sankha. All rights reserved.</p>
+      </div>
+    </div>
+  </footer>;
 };

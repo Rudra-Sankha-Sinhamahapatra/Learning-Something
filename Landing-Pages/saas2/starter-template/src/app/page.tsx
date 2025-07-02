e358bfc306bfc37749b2ad7218852d5ad3412259
Rudra-Sankha-Sinhamahapatra <rssmp120@gmail.com>
2025-07-02 17:08:47 +0530

    adds: introduction component

--- Landing-Pages/saas2/starter-template/src/app/page.tsx
+++ Landing-Pages/saas2/starter-template/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/sections/Hero";
+import Introduction from "@/sections/Introduction";
 import LogoTicker from "@/sections/LogoTicker";
 
 export default function Home() {
     return <div>
         <Hero/>
         <LogoTicker/>
+        <Introduction/>
     </div>;
 }
--- Landing-Pages/saas2/starter-template/src/sections/Introduction.tsx
+++ Landing-Pages/saas2/starter-template/src/sections/Introduction.tsx
@@ -1,5 +1,17 @@
+"use client"
+import StarIcon from "@/assets/images/star.svg";
+
 const text = `You're racing to create exceptional work, but traditional design tools slow you down with unnecessary complexity and steep learning curves.`;
 
 export default function Introduction() {
-    return <div>Introduction</div>;
+    return <div className="mt-24 flex flex-col justify-center items-center">
+        <div className="flex gap-2 rounded-full w-fit px-2 py-1 border border-lime-400">
+        <StarIcon className="fill-lime-400 w-5 h-full"/>
+        <p className="text-lime-400">INTRODUCING LAYERS</p>
+    </div>
+    <div className="text-white text-6xl text-center mx-auto w-[80%] md:w-[72%] lg:w-[62%] my-12 overflow-hidden">
+        Your creative process deserves better. <span className="text-gray-400">{text}</span> 
+        <span className="text-lime-500">That's why we built Layers</span>
+    </div>
+    </div>
 }

e410307efedb47ca94435dd2302e96dedc143b37
Rudra-Sankha-Sinhamahapatra <rssmp120@gmail.com>
2025-07-29 20:05:23 +0530

    wait groups

---
+++ golang/Go-practice/weighting-group/app1/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	for i := range 5 {
+		wg.Add(1)
+		fmt.Println("outer", i)
+		go func() {
+			for i := range 5 {
+				fmt.Println("inner", i)
+			}
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println("Done")
+}

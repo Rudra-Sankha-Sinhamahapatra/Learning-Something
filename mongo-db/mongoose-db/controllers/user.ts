d6d8d62a3bf6d16f3dec77aa231a765d39cff84d
Rudra-Sankha-Sinhamahapatra <rssmp120@gmail.com>
2026-04-25 11:00:06 +0530

    feat(aggregate-fn): adds unwind

--- mongo-db/mongoose-db/controllers/user.ts
+++ mongo-db/mongoose-db/controllers/user.ts
@@ -2,51 +2,80 @@ import type { Request, Response } from "express";
 import { User } from "../models/user";
 
 export const createUser = async (req: Request, res: Response) => {
- try {
-    const user = await User.create(req.body);
-    return res.status(201).json(user);
- } catch (error) {
-    return res.status(500).json({ message: "Internal server error" });
- }
+    try {
+        const user = await User.create(req.body);
+        return res.status(201).json(user);
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error" });
+    }
 }
 
 export const getAvgSalaryByCity = async (req: Request, res: Response) => {
-  try {
-    const { city, minSalary } = req.query;
+    try {
+        const { city, minSalary } = req.query;
 
-    const matchStage : any = {};
+        const matchStage: any = {};
 
-    if (city) matchStage.city = city;
-    if (minSalary) matchStage.salary = { $gte: Number(minSalary) };
+        if (city) matchStage.city = city;
+        if (minSalary) matchStage.salary = { $gte: Number(minSalary) };
 
 
-    const result = await User.aggregate([
-        { $match: matchStage },
+        const result = await User.aggregate([
+            { $match: matchStage },
 
-        {
-            $group: {
-                _id: "$city",
-                avgSalary: { $avg: "$salary" },
-                count: { $sum: 1 },
+            {
+                $group: {
+                    _id: "$city",
+                    avgSalary: { $avg: "$salary" },
+                    count: { $sum: 1 },
+                },
             },
-        },
 
-        {
-            $project: {
-                _id: 0,
-                city: "$_id",
-                avgSalary: 1,
-                count: 1
+            {
+                $project: {
+                    _id: 0,
+                    city: "$_id",
+                    avgSalary: 1,
+                    count: 1
+                },
             },
-        },
 
-        {
-            $sort: { avgSalary: -1 },
-        }
-    ]);
+            {
+                $sort: { avgSalary: -1 },
+            }
+        ]);
 
-    return res.status(200).json(result);
-  } catch (error) {
-    return res.status(500).json({ message: "Internal server error" });
-  }
+        return res.status(200).json(result);
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+export const getTopSkills = async (req: Request, res: Response) => {
+    try {
+        const result = await User.aggregate([
+            { $unwind: {"path": "$skills", "preserveNullAndEmptyArrays": true } },
+
+            {
+                $group: {
+                    _id: "$skills",
+                    count: { $sum: 1 },
+                },
+            },
+
+            {
+                $project: {
+                    _id: 0,
+                    skill: "$_id",
+                    count: 1,
+                },
+            },
+
+            { $sort: { count: -1 } },
+        ]);
+
+        return res.status(200).json(result);
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error" });
+    }
 }
--- mongo-db/mongoose-db/routes/user.ts
+++ mongo-db/mongoose-db/routes/user.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createUser, getAvgSalaryByCity } from "../controllers/user";
+import { createUser, getAvgSalaryByCity, getTopSkills } from "../controllers/user";
 
 const router = Router();
 
 router.post("/create", createUser);
 router.get("/avg-salary", getAvgSalaryByCity);
+router.get("/top-skills", getTopSkills);
 
 export default router;
